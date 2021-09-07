4a5defd165f338febb3938f4fffc3a0922cb0a60
oqbah dx <oqbahdx@gmail.com>
2021-09-07 17:05:04 +0200

    add data to database and show it

--- resources/views/todos/todo-edit.blade.php
+++ resources/views/todos/todo-edit.blade.php
@@ -7,25 +7,12 @@
 
 <div class="card-body">
 
-<div class="from" action = "{{ route('todos') }}" method="post">
-@csrf
-
-
-    <div class="form-group">
-      <label for="">Add Todo</label>
-
-
-    </div>
-    <div class="form-gruop">
-        <input type="text" name="todo" id="todo" class="form-control">
-
-
-    </div>
-  <div class="form-group">
-    <button class="btn btn-primary" type="submit">add</button>
-  </div>
-
-</div>
+<form action="{{route('todos')}}" method="POST">
+ @csrf
+    <label for="todo">Add Todo</label>
+    <input type="text" name="todo" id="todo" class="form-control">
+    <button type="submit" class="btn btn-primary">Add</button>
+</form>
 
 </div>
 
